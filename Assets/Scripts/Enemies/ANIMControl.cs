68b569167304cd24efdbd2e651262ba72fdd5a9a
Lara Torrezan <lara_torrezan87@outlook.com>
2023-09-14 10:18:50 -0300

    add enemy attack

--- Assets/Scripts/Enemies/ANIMControl.cs
+++ Assets/Scripts/Enemies/ANIMControl.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ANIMControl : MonoBehaviour
 {
+    public Transform attackPoint;
+    public float radius;
+    public LayerMask playerLayer;
+    
     private Animator _animator;
 
 
@@ -14,5 +18,23 @@ public class ANIMControl : MonoBehaviour
     public void PlayAmin(int value)
     {
         _animator.SetInteger("transition", value);
+    }
+
+    public void Attack()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(attackPoint.position, radius, playerLayer);
+
+        if (hit != null)
+        {
+            Debug.Log("bateu no player");
+        }
+        else
+        {
+            
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.DrawWireSphere(attackPoint.position, radius);
     }
 }
